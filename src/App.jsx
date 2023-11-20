1d626c4a819c25d018173a42955ad30056a9f173
justice jennings <justice.l.jennings@gmail.com>
2023-11-20 16:52:26 -0500

    corrected fetch data and moved to puppyApi component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import puppyBowlApi from './components/puppyApi';
+import PuppyBowlApi from './components/PuppyApi';
 
 function App() {
- const [allPuppyPlayers, setAllPuppyPlayers] = useState([]);
+//  const [allPuppyPlayers, setAllPuppyPlayers] = useState([]);
 
-  useEffect(() => {
- const fetchPupApi = async () => {
-  try {
-    const response = await fetch(puppyBowlApi`/2310-FSA-ET-WEB-FT-SF/players`);
-    const puppyPlayers = await response.json();
-    console.log(response);
-    setAllPuppyPlayers(puppyPlayers);
-  }
-  catch (error) {
-    console.log(`Error Fetching API!`, error);
-   }
-};
+//   useEffect(() => {
+//  const fetchPupApi = async () => {
+//   try {
+//     const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players`);
+//     const puppyPlayers = await response.json();
+//     console.log(response);
+//     setAllPuppyPlayers(puppyPlayers);
+//   }
+//   catch (error) {
+//     console.log(`Error Fetching API!`, error);
+//    }
+// };
 
- fetchPupApi();
-  }, []);
+//  fetchPupApi();
+//   }, []);
 
   return (
     <>
-    <puppyBowlApi/>
-    
+    < PuppyBowlApi />
+
       <div>
 
       </div>
--- src/components/puppyApi.jsx
+++ src/components/puppyApi.jsx
@@ -1,6 +1,35 @@
-const PuppyBowlApi = `https://fsa-puppy-bowl.herokuapp.com/api`;
-// console.log(puppyBowlApi)
+import { useEffect , useState } from "react";
 
+const PuppyBowlApi = () => {
+const url = `https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players`
+
+const [puppyArray, setPuppyArray] = useState([]);
+
+    useEffect(() => {
+ const fetchPups = async() => {
+
+    try {
+    const response = await fetch(url);
+    const result = await response.json();
+    const pupData = result.data;
+    setPuppyArray(pupData);
+    }
+    catch(error) {
+    console.log(`error fetching!`, error)
+    }
+
+ }
+
+ fetchPups();
+ console.log(puppyArray);
+
+ }, []);
+
+    return (
+    <h2>Puppy Roster</h2>
+        {puppyArray.map()}
+    )
+}
 
 
 
